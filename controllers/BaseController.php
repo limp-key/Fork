32cd5804c750b577bb3bb6887552072aaa68b756
duz.valera <duz.valera.od@gmail.com>
2017-05-28 19:27:25 +0300

    fix bug

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -3,13 +3,18 @@
 namespace limpWork\Controllers;
 
 use \limpWork\Bin\Controller\SkeletonController;
+use \limpWork\Models\BaseModel;
 
 class BaseController extends SkeletonController{
 
     public function index(){
 /*	echo "<pre>";
 	var_dump($_SERVER);
-	echo "</pre>";*/
+   echo "</pre>";*/
+	$db = new BaseModel();
+	$db->create_database('test');
+	echo 'test';
+	$db->exec();
 	return $this->view('index.html.php',['cruel world']);
     }
 }
