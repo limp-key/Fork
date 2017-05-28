ba5f5b01ebfe169d17d5e4c923f179f932ccecc4
duz.valera <duz.valera.od@gmail.com>
2017-05-28 19:07:36 +0300

    update

--- bin/DataBase/ConnectionToDataBase.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$MySQLi = new mysqli($Host, $User, $Password, $Database);
-
-unset($Host);
-unset($User);
-unset($Password);
-unset($Database);
--- bin/Model/SkeletonModel.php
+++ bin/Model/SkeletonModel.php
@@ -2,7 +2,20 @@
 
 namespace limpWork\Model;
 
+use \limpWork\DataBase\Connection;
+
 class SkeletonModel{
+
+    public $Query = "";
+
+  //  public $MySQLi;
+
+    public function __construct(){
+//	$MySQLi = new \mysqli($GLOBALS['Host'],
+//			      $GLOBALS['User'],
+//			      $GLOBALS['Password'],
+//			      $GLOBALS['Database']);
+    }
 
     public function param($parameters = array()){
 	for($i = 0; $i < count($parameters); $i++){
@@ -18,34 +31,51 @@ class SkeletonModel{
     public function select($table, $parameters = array()){
 	$table = $this->param([$table]);
 	$parameters = $this->param($parameters);
-	$Query = "SELECT FROM $table";
-	$MySQLi->query($Query);
+	$this->Query = "SELECT FROM $table";
     }
 
     public function update($table, $parameters = array()){
 	$table = $this->param([$table]);
 	$parameters = $this->param($parameters);
-	$Query = "UPDATE SET";
-	$MySQLi->query($Query);
+	$this->Query = "UPDATE SET";
     }
 
     public function insert($table, $parameters = array()){
 	$table = $this->param([$table]);
 	$parameters = $this->param($parameters);
-	$Query = "INSERT INTO $table () VALUES ()";
-	$MySQLi->query($Query);
+
+	$quickQuery = "DESC $table";
+	$ColumnInTable = $MySQLi->query($quickQuery)->fetch_all(1);
+	$ColumnForInsert = array();
+	for($increment = 0; $increment < count($ColumnInTable); $increment++){
+	    $ColumnForInsert = $ColumnInTable[$increment]['Field'];
+	}
+	$ColumnForInsert = implode(',',$ColumnForInsert);
+	
+	$this->Query = "INSERT INTO $table ($ColumnForInsert) VALUES ($parameters)";
     }
 
     public function delete($table, $parameters = array()){
 	$table = $this->param([$table]);
 	$parameters = $this->param($parameters);
-	$Query = "DELETE FROM $table WHERE";
-	$MySQLi->query($Query);
+	$this->Query = "DELETE FROM $table WHERE";
     }
 
-    public function create_table($TableName){
-	$TableName = $MySQLi->escape_string($TableName);
-	$Query = "CREATE DATABASE $TableName";
-	$MySQLi->query($Query);
+    public function where($parameters){
+	//$table = $this->param([$table]);
+	$parameters = $this->param($parameters);
+	$this->Query .= " WHERE $parameters";
+    }
+
+    public function exec(){
+	$MySQLi->query($this->Query);
+    }
+
+    public function create_database($TableName){
+//	echo 'adf';
+//	$TableName = $MySQLi->escape_string($TableName);
+
+	$this->Query = "CREATE DATABASE $TableName";
+	
     }
 }
