b073ac8f1d272d012a8866e6bee6b99184f62fe6
duz.valera <duz.valera.od@gmail.com>
2017-07-05 14:02:24 +0300

    Cookie

--- bin/Cookie/CookieCreate.php
+++ bin/Cookie/CookieCreate.php
@@ -7,8 +7,17 @@ class CookieCreate{
     public function __construct(){
 	if(!empty($_COOKIE)){
 	    foreach ($_COOKIE as &$cookie) {
-		$cookie = strtoupper($cookie);
+		$cookie = htmlentities($cookie, ENT_QUOTES);
 	    }
 	}
+	return $cookie;
+    }
+
+    public function cookie(){
+	//setcookie();
+    }
+
+    public function cookieraw(){
+	//setrawcookie();
     }
 }
