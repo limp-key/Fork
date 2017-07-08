b1376a948c6228d67a9a8c367e7b9ebc8fbb3dd5
duz.valera <duz.valera.od@gmail.com>
2017-07-08 21:52:05 +0300

    Structure|Fork

---
+++ bin/Fork/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fork\Controllers;
+
+use Fork\Controller\SkeletonController;
+
+class __Name__ extends SkeletonController{
+    
+}
---
+++ bin/Fork/Model.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fork\Models;
+
+use Fork\Model\SkeletonModel;
+
+class __Name__ extends SkeletonModel{
+    
+}
