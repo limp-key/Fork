56368b64cd11223736d35230e05c73ff482bc25e
duz.valera <duz.valera.od@gmail.com>
2017-07-08 19:29:39 +0300

    Migration

--- bin/Migration/Base.php
+++ bin/Migration/Base.php
@@ -4,5 +4,22 @@ namespace Fork\Bin\Migration;
 
 trait Base{
     
-  
+    public function table($Name){
+	$this->Query = "CREATE TABLE {$Name}";
+
+	return true;
+    }
+
+    public function database($Name,$Charset = 'DEFAULT CHARSET UTF8'){
+	$Name = $this->MySQLi->param($Name);
+	$this->Query = "CREATE DATABASE {$Name} {$Charset}";
+
+	return true;
+    }
+
+    public function migrate(){
+	$this->MySQLi->query($this->Query);
+
+	return true;
+    }
 }
