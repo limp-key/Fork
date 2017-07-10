954152097d4cd6b086dd0b701d87189157e98a17
duz.valera <duz.valera.od@gmail.com>
2017-07-10 15:35:48 +0300

    Validation

--- bin/Assay/Validation.php
+++ bin/Assay/Validation.php
@@ -3,64 +3,70 @@
 namespace Fork\Bin\Assay;
 
 class Validation{
-    public function email($Parameter){
+    
+    public function Email($Parameter){
 	$RegularEmail = "/^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+$/";
-	if(preg_match_all($RegularEmail,$Parameter)){
-	    return true;
-	}else{
-	    return false;
-	}
+
+	return preg_match_all($RegularEmail,$Parameter);
     }
-    public function login($Parameter){
+    
+    public function Login($Parameter){
 	$RegularLogin = "/^[a-zA-Z]+$/";
-	if(preg_match_all($RegularLogin,$Parameter)){
-	    return true;
-	}else{
-	    return false;
-	}
+
+	return preg_match($RegularLogin,$Parameter);
     }
-    public function password($Parameter,$Length = 8){
-	$RegularPassword = "/^[a-zA-Z0-9_@!+&^%$#()?]+$/";
-	if(strlen($Parameter) >= $Length){
-	    return true;
-	}else{
-	    return false;
-	}
+    
+    public function Password($Parameter,$Length = 8){
+	$RegularPassword = "/^[a-zA-Z0-9_@!+&^%$#()?]{".$Lenght.",}$/";
+
+	return preg_match($RegularPassword,$Parameter);
     }
 
-    public function url($Parameter){
+    public function URL($Parameter){
 	$RegularURL = "#(https:\/\/)?(www\.)?([-а-яa-z0-9_\.]{2,}\.)([a-z]{2,6})((\/[-а-яa-z0-9_]{1,})?\/?([a-z0-9_-]{2,}\.[a-z]{2,6})?(\?[a-z0-9_]{2,}=[-0-9]{1,})?((\&[a-z0-9_]{2,}=[-0-9]{1,}){1,})?)#i";
-	if(preg_match_all($RegularURL,$Parameter)){
-	    return true;
-	}else{
-	    return false;
-	}
+
+	return preg_match($RegularURL,$Parameter);
     }
     
-    public function phone(){
+    public function Phone($Parameter){
     	$RegularPhone = "##i";
-	if(preg_match_all($RegularPhone,$Parameter)){
-	    return true;
-	}else{
-	    return false;
-	}
+
+	return preg_match($RegularPhone,$Parameter);
     }
     
-    public function date(){
+    public function Date($Parameter){
+    	$RegularDate = "#()#i"; // 2222.22.22 22.22.2222
+
+	return preg_match($RegularDate,$Parameter);
+    }
+
+    public function Time($Parameter){
     	$RegularDate = "##i";
-	if(preg_match_all($RegularDate,$Parameter)){
-	    return true;
-	}else{
-	    return false;
-	}
+
+	return preg_match($RegularDate,$Parameter);
+    }
+
+    public function DateTime($Parameter){
+    	$RegularDate = "##i";
+
+	return preg_match($RegularDate,$Parameter);
     }
     
-    public function file(){
-	    $RegularFile = "##i";
-	if(preg_match_all($RegularFile,$Parameter)){
-	    return true;
-	}else{
-	    return false;
-	}
+    public function File($Parameter){
+	$RegularFile = "#^([a-z0-9A-Z]).([a-z])$#i";
+
+	return preg_match($RegularFile,$Parameter)
+    }
+
+    public function IPv4($Parameter){
+	$RegularIPv4 = "#^\d{1,3}.\d{1,3}.\d{1.3}.\d{1,3}$#i";
+
+	return preg_match($RegularFile,$Parameter);
+    }
+
+    public function IPv6($Parameter){
+	$RegularIPv6 = "#^[a-z0-9]{1,4}:[a-z0-9]{1,4}:[a-z0-9]{1,4}:[a-z0-9]{1,4}:[a-z0-9]{1,4}:[a-z0-9]{1,4}:[a-z0-9]{1,4}:[a-z0-9]{1,4}$#i";
+
+	return preg_match($RegularFile,$Parameter);
     }
 }
