0f09edadd1b90138d6e65851c2bad23bb343c6f4
limp-key <duz.valera.od@gmail.com>
2017-07-16 19:12:06 +0300

    Session

--- bin/Modules/Session/SessionDatabase.php
+++ bin/Modules/Session/SessionDatabase.php
@@ -2,25 +2,55 @@
 
 namespace Fork\Bin\Modules\Session;
 
-class SessionDatabase{
-    public function __construct($SessionName = null){
+interface SessionHandlerInterface {
+    
+    abstract public bool Close ( void );
+    
+    abstract public bool Destroy ( string $SessionID );
+    
+    abstract public bool GC ( int $MaxLifeTime );
+    
+    abstract public bool Open ( string $SavePath , string $SessionName );
+    
+    abstract public string Read ( string $SessionID );
+    
+    abstract public bool Write ( string $SessionID , string $SessionData );
+}
 
 
+class SessionHandler extends SessionHandlerInterface{
+
+    public function __construct(){
+	
     }
-
-    public function __destruct(){
+    
+    public function Close(){
 	
     }
 
-    public function write($Session,$Parameter){
+    public function Destroy(){
 	
     }
 
-    public function read($ParametersInSession){
+    public function GC(){
 	
     }
 
-    public function destroy(){
+    public function Open(){
+	
+    }
 
+    public function Read(){
+	
+    }
+
+    public function Write(){
+	
     }
 }
+
+
+#ini_set('session.save_handler', 'files');
+#$Handler = new SessionHandler();
+#session_set_save_handler($Handler, true);
+#session_start();
