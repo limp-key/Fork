f196d8d519f5719868c5a3646ef3c8acd647e07a
limp-key <duz.valera.od@gmail.com>
2017-07-17 16:32:28 +0300

    Config

--- bin/Config/SkeletSystemConfig.php
+++ bin/Config/SkeletSystemConfig.php
@@ -4,6 +4,466 @@ namespace Fork\Bin\Config;
 
 class SkeletSystemConfig{
 
+    public $precision ;
+    
+    public $output_buffering ;
+
+
+    public $output_handler;
+
+    public $url_rewriter_tags;
+
+    public $url_rewriter_hosts;
+    
+
+    public $zlib_output_compression;
+
+    public $zlib_output_compression_level;
+
+    public $zlib_output_handler;
+
+    
+    public $implicit_flush;
+
+    public $unserialize_callback_func;
+    
+    public $serialize_precision;
+
+    public $open_basedir;
+
+    public $disable_functions;
+    
+    public $disable_classes;
+
+    public $ignore_user_abort;
+
+    public $realpath_cache_size;
+
+    public $realpath_cache_ttl;
+
+    public $zend_enable_gc;
+
+    public $zend_multibyte;
+
+    public $zend_script_encoding;
+
+    public $expose_php;
+
+    public $max_execution_time;
+
+    public $max_input_time;
+
+    public $max_input_nesting_level;
+
+    public $max_input_vars;
+
+    public $memory_limit;
+
+    public $error_reporting;
+
+    public $display_errors;
+
+    public $display_startup_errors;
+
+    public $log_errors;
+
+    public $log_errors_max_len;
+
+    public $ignore_repeated_errors;
+
+    public $ignore_repeated_source;
+
+    public $report_memleaks;
+
+    public $track_errors;
+
+    public $xmlrpc_errors;
+
+    public $xmlrpc_error_number;
+
+    public $html_errors;
+
+    public $error_prepend_string;
+
+    public $error_append_string;
+
+    public $error_log;
+
+    public $error_log;
+
+    public $arg_separator_output;
+
+    public $arg_separator_input;
+
+    public $variables_order;
+
+    public $request_order;
+
+    public $register_argc_argv;
+
+    public $auto_globals_jit;
+
+    public $enable_post_data_reading;
+
+    public $post_max_size;
+
+    public $auto_prepend_file;
+
+    public $auto_append_file;
+    
+    public $default_mimetype;
+
+    public $default_charset;
+
+    public $internal_encoding;
+
+    public $input_encoding;
+
+    public $output_encoding;
+
+    public $include_path;
+
+    public $doc_root;
+    
+    public $user_dir;
+
+    public $extension_dir;
+    
+    public $sys_temp_dir;
+
+    public $enable_dl;
+
+    public $cgi_force_redirect;
+
+    public $cgi_nph;
+
+    public $cgi_redirect_status_env;
+
+    public $cgi_fix_pathinfo;
+
+    public $cgi_discard_path;
+
+    public $fastcgi_impersonate;
+
+    public $fastcgi_logging;
+
+    public $cgi_rfc2616_headers;
+
+    public $cgi_check_shebang_line;
+
+    public $file_uploads;
+
+    public $upload_tmp_dir;
+
+    public $upload_max_filesize;
+
+    public $max_file_uploads;
+
+    public $allow_url_fopen;
+
+    public $allow_url_include;
+
+    public $from;
+
+    public $user_agent;
+
+    public $default_socket_timeout;
+
+    public $auto_detect_line_endings;
+
+    public $cli_server_color;
+
+    public $date_timezone;
+
+    public $date_default_latitude;
+
+    public $date_default_longitude;
+
+    public $date_sunrise_zenith;
+
+    public $date_sunset_zenith;
+
+    public $filter_default;
+
+    public $filter_default_flags;
+
+    public $iconv_input_encoding;
+
+    public $iconv_internal_encoding;
+
+    public $iconv_output_encoding;
+
+    public $intl_default_locale;
+    
+    public $intl_error_level;
+    
+    public $intl_use_exceptions;
+
+    public $sqlite3_extension_dir;
+
+    public $pcre_backtrack_limit;
+
+    public $pcre_recursion_limit;
+
+    public $pcre_jit;
+
+    public $pdo_odbc_connection_pooling;
+
+    public $pdo_odbc_db2_instance_name;
+
+    public $pdo_mysql_cache_size;
+
+
+    public $pdo_mysql_default_socket;
+
+    public $phar_readonly;
+    
+    public $phar_require_hash;
+
+    public $phar_cache_list;
+    
+
+    public $SMTP;
+
+    public $smtp_port;
+
+    public $sendmail_path;
+
+    public $mail_force_extra_parameters;
+
+    public $mail_add_x_header;
+
+    public $mail_log;
+
+    public $mail_log;
+
+    public $sql_safe_mode;
+
+    public $odbc_default_db;
+
+    public $odbc_default_user;
+
+    public $odbc_default_pw;
+
+    public $odbc_default_cursortype;
+
+    public $odbc_allow_persistent;
+
+    public $odbc_check_persistent;
+
+    public $odbc_max_persistent;
+
+    public $odbc_max_links;
+
+    public $odbc_defaultlrl;
+
+    public $odbc_defaultbinmode;
+
+    public $birdstep_max_links;
+
+    public $ibase_allow_persistent;
+
+    public $ibase_max_persistent;
+
+    public $ibase_max_links;
+
+    public $ibase_default_db;
+
+    public $ibase_default_user;
+
+    public $ibase_default_password;
+
+    public $ibase_default_charset;
+
+    public $ibase_timestampformat;
+
+    public $ibase_dateformat;
+
+    public $ibase_timeformat;
+
+    public $mysqli_max_persistent;
+
+    public $mysqli_allow_local_infile;
+
+    public $mysqli_allow_persistent;
+
+    public $mysqli_max_links;
+
+    public $mysqli_cache_size;
+
+    public $mysqli_default_port;
+
+    public $mysqli_default_socket;
+
+    public $mysqli_reconnect;
+
+    public $mysqlnd_collect_statistics;
+
+    public $mysqlnd_collect_memory_statistics;
+
+    public $mysqlnd_debug;
+
+    public $mysqlnd_log_mask;
+
+    public $mysqlnd_mempool_default_size;
+
+    public $mysqlnd_net_cmd_buffer_size;
+
+    public $mysqlnd_net_read_buffer_size;
+
+    public $mysqlnd_net_read_timeout;
+
+    public $mysqlnd_sha256_server_public_key;
+
+    public $oci8_privileged_connect;
+
+    public $oci8_max_persistent;
+
+    public $oci8_persistent_timeout;
+
+    public $oci8_ping_interval;
+
+    public $oci8_connection_class;
+
+    public $oci8_events;
+
+    public $oci8_statement_cache_size;
+
+    public $oci8_default_prefetch;
+
+    public $oci8_old_oci_close_semantics;
+
+    public $pgsql_allow_persistent;
+
+    public $pgsql_auto_reset_persistent;
+
+    public $pgsql_max_persistent;
+
+    public $pgsql_max_links;
+
+    public $pgsql_ignore_notice;
+
+    public $pgsql_log_notice;
+
+    public $bcmath_scale;
+
+    public $session_save_handler;
+
+    public $session_save_path;
+
+    public $session_use_strict_mode;
+
+    public $session_use_cookies;
+
+    public $session_cookie_secure;
+
+    public $session_use_only_cookies;
+
+    public $session_name;
+
+    public $session_auto_start;
+
+    public $session_cookie_lifetime;
+
+    public $session_cookie_path;
+
+    public $session_cookie_domain;
+    
+    public $session_cookie_httponly;
+    
+    public $session_serialize_handler;
+
+    public $session_gc_probability;
+
+    public $session_gc_divisor;
+
+    public $session_gc_maxlifetime;
+
+    public $session_referer_check;
+
+    public $session_cache_limiter;
+
+    public $session_cache_expire;
+
+    public $session_use_trans_sid;
+
+    public $session_sid_length;
+
+    public $session_trans_sid_tags;
+
+    public $session_trans_sid_hosts;
+
+    public $session_sid_bits_per_character;
+
+    public $session_upload_progress_enabled;
+
+    public $session_upload_progress_cleanup;
+
+    public $session_upload_progress_prefix;
+
+    public $session_upload_progress_name;
+
+    public $session_upload_progress_freq;
+
+    public $session_upload_progress_min_freq;
+
+    public $session_lazy_write;
+
+    public $zend_assertions;
+
+    public $assert_active;
+
+    public $assert_exception;
+
+    public $assert_warning;
+
+    public $assert_bail;
+
+    public $assert_callback;
+
+    public $assert_quiet_eval;
+
+    public $mbstring_language;
+
+    public $mbstring_internal_encoding;
+
+    public $mbstring_http_input;
+
+    public $mbstring_http_output;
+
+    public $mbstring_encoding_translation = 'Off'
+
+    public $mbstring_detect_order;
+
+    public $mbstring_substitute_character;
+
+    public $mbstring_func_overload;
+
+    public $mbstring_strict_detection;
+
+    public $mbstring_http_output_conv_mimetype;
+
+    public $tidy_default_config;
+
+    public $tidy_clean_output;
+
+    public $soap_wsdl_cache_enabled;
+
+    public $soap_wsdl_cache_dir;
+
+    public $soap_wsdl_cache_ttl;
+
+    public $soap_wsdl_cache_limit;
+
+    public $sysvshm_init_mem;
+
+    public $ldap_max_links;
+
+    public $mcrypt_algorithms_dir;
+
+    public $mcrypt_modes_dir;
+    
+
     public function __construct(){
 	
 	$this->Precision($this->precision);
@@ -475,71 +935,278 @@ class SkeletSystemConfig{
 	#$this->$mcrypt_algorithms_dir=
 
 	#$this->$mcrypt_modes_dir=
-
-	#$this->$opcache_enable=1
-
-	#$this->$opcache_enable_cli=1
-
-	#$this->$opcache_memory_consumption=128
-
-	#$this->$opcache_interned_strings_buffer=8
-
-	#$this->$opcache_max_accelerated_files=10000
-
-	#$this->$opcache_max_wasted_percentage=5
-
-	#$this->$opcache_use_cwd=1
-
-	#$this->$opcache_validate_timestamps=1
-
-	#$this->$opcache_revalidate_freq=2
-
-	#$this->$opcache_revalidate_path=0
-
-	#$this->$opcache_save_comments=1
-
-	#$this->$opcache_fast_shutdown=0
-
-	#$this->$opcache_enable_file_override=0
-
-	#$this->$opcache_optimization_level=0xffffffff
-
-	#$this->$opcache_inherited_hack=1
-
-	#$this->$opcache_dups_fix=0
-
-	#$this->$opcache_blacklist_filename=
-
-	#$this->$opcache_max_file_size=0
-
-	#$this->$opcache_consistency_checks=0
-
-	#$this->$opcache_force_restart_timeout=180
-
-	#$this->$opcache_error_log=
-
-	#$this->$opcache_log_verbosity_level=1
-
-	#$this->$opcache_preferred_memory_model=
-
-	#$this->$opcache_protect_memory=0
-
-	#$this->$opcache_restrict_api=
-
-	#$this->$opcache_mmap_base=
-
-	#$this->$opcache_file_cache=
-
-	#$this->$opcache_file_cache_only=0
-
-	#$this->$opcache_file_cache_consistency_checks=1
-
-	#$this->$opcache_file_cache_fallback=1
-
-	#$this->$opcache_huge_code_pages=1
-
-	#$this->$opcache_validate_permission=0
-
-	#$this->$opcache_validate_root=0
     }
+
+
+
+
+
+
+    
+    
+    short_open_tag=On
+    
+    asp_tags=Off
+
+    precision=14
+
+    y2k_compliance=Off
+
+    output_buffering=Off
+
+    implicit_flush=Off
+
+    allow_call_time_pass_reference=On
+    
+    safe_mode=Off
+    
+    safe_mode_exec_dir=
+	
+	safe_mode_allowed_env_vars=PHP_
+
+    safe_mode_protected_env_vars=LD_LIBRARY_PATH
+
+    disable_functions=
+
+				
+				expose_php=On
+    
+    max_execution_time=30
+    memory_limit=8M
+
+
+    error_reporting= E_ALL
+
+    display_errors=On
+
+    display_startup_errors=On
+
+    log_errors=Off
+
+    track_errors=On
+
+    #error_prepend_string="<font color=ff0000>"
+
+    #error_append_string="</font>"
+
+    #error_log=filename#
+
+    #error_log=syslog
+
+    warn_plus_overloading=Off
+
+    variables_order="EGPCS"
+
+    register_globals=On
+
+    register_argc_argv=On
+
+    post_max_size=8M
+
+    gpc_order="GPC"
+
+    magic_quotes_gpc=Off
+
+    magic_quotes_runtime=Off
+
+    magic_quotes_sybase=Off
+
+    auto_prepend_file= '';
+
+    auto_append_file= '';
+    
+    default_mimetype="text/html"
+
+    default_charset="iso-8859-1"
+
+    
+    include_path= '';
+
+    doc_root= '';
+    user_dir= '';
+
+    enable_dl=On
+
+    
+
+    file_uploads=On
+
+    #upload_tmp_dir=
+
+    upload_max_filesize=2M
+
+    
+    
+    define_syslog_variables=Off
+
+    #sendmail_path=
+
+    #debugger.host=localhost
+    #debugger.port=7869
+    #debugger.enabled=False
+
+    #logging.method=db
+    #logging.directory=/path/to/log/directory
+
+
+    [SQL]
+
+    sql.safe_mode=Off
+
+    [ODBC]
+
+    #uodbc.default_db=Not yet implemented
+    #uodbc.default_user=Not yet implemented
+    #uodbc.default_pw=Not yet implemented
+    # Разрешает или запрещает устойчивые соединения
+    uodbc.allow_persistent=On
+    # Проверка доступности соединения перед его использованием.
+    uodbc.check_persistent=On
+    # Макс. число устойчивых соединений. -1 означает, что ограничений нет.
+    uodbc.max_persistent=-1
+    # Макс. число соединений (устойчивых + неустойчивых).
+    uodbc.max_links=-1
+    # Установки для LONG-полей.
+    uodbc.defaultlrl=4096
+    # Установки для бинарных данных. 0 означает режим passthru, 1 – режим
+    # as is, 2 – преобразование в символы.
+    uodbc.defaultbinmode=1
+    # См. документацию по odbc_binmode и odbc_longreadlen для более
+    # детального разъяснения смысла директив uodbc.defaultlrl и
+    # uodbc.defaultbinmode.
+
+    [MySQL]
+
+    mysql.allow_persistent=On
+    mysql.max_persistent=-1
+    mysql.max_links=-1
+    # Порт по умолчанию для функции mysql_connect(). Если не задан, функция
+    # попытается использовать переменную $MYSQL_TCP_PORT или запись mysql-tcp
+    # в /etc/services, а также заданную во время компиляции PHP константу
+    # MYSQL_PORT (именно в таком порядке). К PHP для Win32 применимо только
+    # последнее.
+    mysql.default_port= '';
+	# Определяет имя сокета для локальных соединений MySQL. Если он не задан,
+	# использует встроенное значение по умолчанию.
+    mysql.default_socket= '';
+
+    [PostgresSQL]
+
+    pgsql.allow_persistent=On
+    pgsql.max_persistent=-1
+    pgsql.max_links=-1
+
+    [Sybase]
+
+    sybase.allow_persistent=On
+    sybase.max_persistent=-1
+    sybase.max_links=-1
+    #sybase.interface_file="/usr/sybase/interfaces"
+    # Максимальный уровень серьезности отображаемых ошибок.
+    sybase.min_error_severity=10
+    # Минимальный уровень серьезности отображаемых ошибок.
+    sybase.min_message_severity=10
+    # Режим совместимости со старыми версиями PHP 3.0.
+    # Если следующая директива установлена в On, PHP будет автоматически
+    # присваивать тип результату на основе его типа в Sybase, вместо того,
+    # чтобы преобразовывать полученные значения в строки. Этот режим
+    # совместимости, возможно, в будущем не будет поддерживаться, так что
+    # лучше исправьте свои сценарии, если вам он нужен.
+    sybase.compatability_mode=Off
+
+    [Sybase-CT]
+
+    sybct.allow_persistent=On
+    sybct.max_persistent=-1
+    sybct.max_links=-1
+    sybct.min_server_severity=10
+    sybct.min_client_severity=10
+
+    [Informix]
+
+    ifx.default_host= '';
+    ifx.default_user= '';
+    ifx.default_password= '';
+    ifx.allow_persistent='On';
+    ifx.max_persistent=-1
+    ifx.max_links=-1
+    # Если следующая директива установлена в On, выражение select возвращает
+    # содержимое поля типа text blob вместо его идентификатора.
+    ifx.textasvarchar=0
+    # Заставляет команду select возвращать значение поля типа byte blob
+    # вместо его идентификатора.
+    ifx.byteasvarchar=0
+    # Принуждает PHP удалять завершающие пробелы из колонок с типом char
+    # фиксированного размера. Может помочь пользователям Informix SE.
+    ifx.charasvarchar=0
+    # Если установлена, содержимое полей text и byte сохраняется в файле,
+    # вместо того, чтобы храниться в памяти.
+    ifx.blobinfile=0
+    # Если установлена в 0, значения NULL возвращаются как пустые строки,
+    # иначе они возвращаются как строки 'NULL'.
+    ifx.nullformat=0
+
+    [Session]
+
+    # Определяет режим хранения данных сессий.
+    session.save_handler=files
+    # Следующая директива задает аргумент, передаваемый save_handler-у.
+    # В случае режима сохранения в файлах здесь должен указываться каталог,
+    # в который будут помещены файлы сессий.
+    session.save_path=C:\Program Files\PHP4\sessiondata
+    # Должен ли PHP использовать Cookies.
+    session.use_cookies=1
+    session.name=PHPSESSID
+    # Инициализировать ли сессии при старте.
+    session.auto_start=0
+    # Время жизни Cookie для сессии. Если до закрытия браузера, то 0.
+    session.cookie_lifetime=0
+    # Путь для Cookie с идентификатором сессии.
+    session.cookie_path=/
+    # Домен для Cookie с идентификатором сессии.
+    session.cookie_domain=
+	# Функция, используемая для сериализации данных. Значение php задает
+	# стандартную функцию.
+	session.serialize_handler=php
+    # Вероятность того, что при очередном запуске сценария, работающего с
+    # сессиями, будет вызвана функция "сборки мусора" для очистки сессий,
+    # которые пользователь уже покинул.
+    session.gc_probability=1
+    # После указанного здесь промежутка времени сохраненные
+    # данные будут удалены автоматически сборщиком мусора.
+    session.gc_maxlifetime=1440
+    # Проверять ли HTTP Referer на предмет того, не является ли ID сессии
+    # "фальшивым".
+    session.referer_check=
+	# Указывает, сколько байтов читать из файла.
+	session.entropy_length=0
+    #session.entropy_length=16
+    # Файл, используемый для генерации идентификаторов сессии.
+    session.entropy_file=
+	#session.entropy_file=/dev/urandom
+	# Установите одно из значений nocache, private, public для определения
+	# аспектов кэширования HTTP.
+	session.cache_limiter=nocache
+    # Документ будет считаться устаревшим по истечении заданного
+    # здесь количества минут
+    session.cache_expire=180
+    # Использовать ли поддержку "переходящих" SID. Действует, если PHP был
+    # скомпилирован с включенной опцией --enable-trans-sid.
+    session.use_trans_sid=1
+
+    [Assertion]
+
+    #assert.active=On
+    # Генерирует предупреждения PHP для каждых неудавшихся проверок
+    # выражений.
+    #assert.warning=On
+    # По умолчанию не завершать программу в случае неудачи.
+    #assert.bail=Off
+    # Пользовательская функция, которая будет вызвана при неудаче проверки.
+    #assert.callback=0
+    # Вычислять выражения в eval с использованием текущих установок
+    # error_reporting. Установите в true, если вы хотите, чтобы действие
+    # режима error_reporting(0) было сохранено и при переходе через
+    # границу eval().
+    #assert.quiet_eval=0
 }
