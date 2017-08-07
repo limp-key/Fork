8674e118fddbd5114ef1a8c88f34b4cd4b1af2d1
limp-key <duz.valera.od@gmail.com>
2017-08-07 11:07:36 +0300

    Config

--- config/File.config.php
+++ config/File.config.php
@@ -6,16 +6,16 @@ use \Fork\Bin\Config\SkeletFileConfig;
 
 class FileConfig extends SkeletFileConfig{
 
-    public static $FileUploads = 0;
+    #public static $FileUploads = 0;
 
-    public static $UploadTmpDir = '/var/www/';
+    #public static $UploadTmpDir = '/var/www/';
 
-    public static $UploadMaxFilesize = '4M';
+    #public static $UploadMaxFilesize = '4M';
 
-    public static $MaxFileUploads = '20';
+    #public static $MaxFileUploads = '20';
 
-    public static $AutoPrependFile = 'adf';
+    #public static $AutoPrependFile = 'adf';
     
-    public static $AutoAppendFile = 'adf';
+    #public static $AutoAppendFile = 'adf';
 
 }
